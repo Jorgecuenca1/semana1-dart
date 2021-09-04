ff0074f4cc6f0391f4c1bd36fa39fcd1b4039659
jorge <jorgecuenca.unity@gmail.com>
2021-09-04 08:41:42 -0500

    second

---
+++ 2.tipos/tipos.dart
@@ -0,0 +1,21 @@
+void main() {
+  int edadInt = 23;
+  num puntosDouble = 10.2;
+  num edadNum = puntosDouble;
+  print(edadNum);
+
+  var texto1 = 'valor con commiilla simple';
+  var texto2 = "valor comillas dobles";
+  var texto3 = "jjsjdjsjdjsjsdjsdjjsdjsd";
+  var interpolacion = "hola como estan esto es : ${texto2}";
+  var texto6 = "este vlor se ne con el siguiente  " + "nnn";
+  var texto7 = "me uno con " ",este otro valor";
+  var VariableTipoNumero = 50;
+  var texto9 =
+      "interpolar texo 7 \'$texto7\' el valor de la variable tipo nuemero es $VariableTipoNumero";
+
+  print(texto1);
+  print(texto2);
+  print(texto3);
+  print(interpolacion);
+}
