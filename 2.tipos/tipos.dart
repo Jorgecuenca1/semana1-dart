a2f2d99bdcae067928dd88d467375cd987a67878
jorge <jorgecuenca.unity@gmail.com>
2021-09-04 09:01:04 -0500

    third

--- 2.tipos/tipos.dart
+++ 2.tipos/tipos.dart
@@ -13,9 +13,25 @@ void main() {
   var VariableTipoNumero = 50;
   var texto9 =
       "interpolar texo 7 \'$texto7\' el valor de la variable tipo nuemero es $VariableTipoNumero";
-
+  var texto10 = '''CREATE TABLE Persons(
+                  PersonID int,
+                  LastName varchar(255),
+                  FirstName varchar(255),
+                  );''';
+  var texto11 = r'mostrar este caracter \';
+  var texto12 = '';
+  if (texto12.isEmpty) {
+    print("holaaaa");
+  }
+  var numero1 = 4;
+  if (numero1 >= 5) {
+    print("decir es igual o mayor a 5");
+  }
   print(texto1);
   print(texto2);
   print(texto3);
   print(interpolacion);
+  print(texto9);
+  print(texto10);
+  print(texto11);
 }
