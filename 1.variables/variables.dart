374855e2c688639ca8ef4abb30eb0c838043071b
jorge <jorgecuenca.unity@gmail.com>
2021-09-04 08:19:21 -0500

    nuevo

---
+++ 1.variables/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  var nombre = "juan";
+  String nombre_dos = "juan";
+  var edad = [89.0];
+  var i = 42;
+
+  final String variableFinalTest = "hola";
+  const String variableConstanteTest = "hola";
+  print(nombre_dos.toUpperCase());
+  print(nombre);
+}
