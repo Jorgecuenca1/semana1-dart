309e39bf972b22fb93bd44953f9a762afcb2bc9c
jorge <jorgecuenca.unity@gmail.com>
2021-09-10 15:31:23 -0500

    trabajo

--- 7.ejercicios/ejercicio5.dart
+++ 7.ejercicios/ejercicio5.dart
@@ -1,50 +1,159 @@
-class Persona {
-  String name;
-  String lastname;
-  String adress;
-  int phone;
-
-  Persona(this.name, this.lastname, this.adress, this.phone);
-  Persona.completo(
-      {this.name = 'name',
-      this.lastname = 'apellido',
-      this.adress = "adress",
-      this.phone = 60});
-}
-
-class Artista extends Persona {
-  int? pinturas;
-  String? tecnicas;
-
-  Artista({this.pinturas, this.tecnicas})
-      : super.completo(name = "name", lastname = "lastname", adress = "adress",
-            phone = 60, pinturas = 3, tecnicas = "varias");
-}
-
-class Ingeniero extends Persona {
-  int? experiencia;
-  String? lenguaje;
-
-  Ingeniero({this.experiencia, this.lenguaje})
-      : super.completo(name = "name", lastname = "lastname", adress = "adress",
-            phone = 60, experiencia = 3, lenguaje = "python");
-}
-
-class Doctor extends Persona {
-  int? titulos;
+mixin Dibujar {
+  int? tecnica_dibujo;
   String? especialidad;
 
-  Doctor({this.titulos, this.especialidad})
-      : super.completo(name = "name", lastname = "lastname", adress = "adress",
-            phone = 60, titulos = 3, especialidad = "cirujano");
+  tipoDibujo() {
+    return "Dibujar";
+  }
+}
+mixin Leer {
+  int? numlibros;
+  String? categoria;
+
+  tipoLectura() {
+    return "Leer";
+  }
+}
+mixin Ejercitarse {
+  int? tiempo_ejercicio;
+  String? tipo_ejercicio;
+
+  tipoEjercicio() {
+    return "Ejercitarse";
+  }
+}
+mixin Boxear {
+  int? tiempo_entrenamiento;
+  String? tipo_boxeo;
+
+  tipoEjercicio() {
+    return "Boxear";
+  }
 }
 
-class Atleta extends Persona {
+abstract class Persona {
+  String? name;
+  String? lastname;
+  String? adress;
+  int? phone;
+
+  String nameComplete() {
+    return "$name $lastname";
+  }
+
+  String trabajo();
+  Persona(this.name, this.lastname, this.adress, this.phone);
+}
+
+class Artista extends Persona with Dibujar {
+  int? pinturas;
+  String? tecnicas;
+  String? categoria;
+
+  Artista(String name, String lastname, String adress, int phone)
+      : super(name, lastname, adress, phone);
+  @override
+  String trabajo() {
+    return "Artista";
+  }
+
+  String tecnicapinturas() {
+    return "Tecanica: $tecnicas";
+  }
+}
+
+class Ingeniero extends Persona with Dibujar, Leer {
+  int? experiencia;
+  String? lenguaje;
+  String? categoria;
+
+  Ingeniero(String name, String lastname, String adress, int phone)
+      : super(name, lastname, adress, phone);
+  String trabajo() {
+    return "Ingeniero";
+  }
+
+  String lenguajeprogramacion() {
+    return "Lenguaje: $lenguaje";
+  }
+}
+
+class Doctor extends Persona with Leer, Ejercitarse {
+  int? titulos;
+  String? especialidad_medica;
+  String? categoria;
+
+  Doctor(String name, String lastname, String adress, int phone)
+      : super(name, lastname, adress, phone);
+  String trabajo() {
+    return "Doctor";
+  }
+
+  String especialidadmedica() {
+    return "Especialidad: $especialidad_medica";
+  }
+}
+
+abstract class Atleta extends Persona {
   String? deporte;
+  int? medallas;
+  int? campeonatos;
 
-  Atleta({this.deporte})
-      : super.completo(name = "name", lastname = "lastname", adress = "adress",
-            phone = 60, deporte = "atletismo");
+  Atleta(String name, String lastname, String adress, int phone)
+      : super(name, lastname, adress, phone);
+  String esatleta();
+  String numeromedallas() {
+    return "Medallas: $medallas";
+  }
 }
 
-void main() {}
+class Boxeador extends Persona with Ejercitarse, Boxear {
+  String? liga;
+  String? representante;
+  int? talla;
+
+  Boxeador(String name, String lastname, String adress, int phone)
+      : super(name, lastname, adress, phone);
+  String trabajo() {
+    return "Boxeador";
+  }
+
+  String representantedelboxeador() {
+    return "representante: $representante";
+  }
+
+  String esatleta() {
+    return "SI es Atleta";
+  }
+}
+
+void main() {
+  Artista artista = Artista("Jorge", "Hernan", "santa Helana", 3102337052);
+  artista.pinturas = 2;
+  artista.tecnicas = "tecnica 1";
+  artista.categoria = "categoria1";
+  print(artista.trabajo());
+  print(artista.tecnicapinturas());
+
+  Ingeniero ingeniero =
+      Ingeniero("Jorge", "Hernan", "santa Helana", 3102337052);
+  ingeniero.experiencia = 2;
+  ingeniero.lenguaje = "java";
+  ingeniero.categoria = "categoria1";
+  print(ingeniero.trabajo());
+  print(ingeniero.lenguajeprogramacion());
+
+  Doctor doctor = Doctor("Jorge", "Hernan", "santa Helana", 3102337052);
+  doctor.titulos = 2;
+  doctor.especialidad_medica = "cirujano 1";
+  doctor.categoria = "categoria1";
+  print(doctor.trabajo());
+  print(doctor.especialidadmedica());
+
+  Boxeador boxeador = Boxeador("Jorge", "Hernan", "santa Helana", 3102337052);
+  boxeador.talla = 2;
+  boxeador.liga = "tecnica 1";
+  boxeador.representante = "representante";
+  print(boxeador.trabajo());
+  print(boxeador.representantedelboxeador());
+}
